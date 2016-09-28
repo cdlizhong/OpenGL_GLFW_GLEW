f0ca91d34c042eea9a38a390e15ffdf7572d3cd8
cdlizhong <cdlizhong@163.com>
2016-09-28 08:15:39 +0800

    Initial Commit

---
+++ openGL_GLFW_GLEW/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL_GLFW_GLEW
+//
+//  Created by cdlizhong on 16/9/28.
+//  Copyright © 2016年 cdlizhong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
